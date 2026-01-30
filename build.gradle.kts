e33c3626948f4f881b67a6fc0c3fa5684a482598
noahffiliation <nlindsey15@gmail.com>
2026-01-30 00:06:34 -0600

    Security fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,13 @@ buildscript {
             force(libs.protobuf.java)
             force(libs.protobuf.kotlin)
             force(libs.jdom2)
+            force(libs.netty.common)
+            force(libs.netty.handler)
+            force(libs.netty.codec.http)
             force(libs.netty.codec.http2)
+            force(libs.jose4j)
+            force(libs.commons.lang3)
+            force(libs.httpclient)
         }
     }
 }
